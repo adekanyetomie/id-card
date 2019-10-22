4f8641a2650bbc0eb8e477f9d2b0bdd7e60bf759
adekanyetomie <adekanyetomy@gmail.com>
2019-10-22 03:49:37 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Id() ,
+));
+class Id extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blueGrey[900],
+      appBar: AppBar(
+        title: Text('Identification Card'),
+        centerTitle: true,
+        elevation: 0.0,
+        backgroundColor: Colors.blueGrey[700],
+
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Center(
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/image.jpg'),
+                radius: 50.0,
+              ),
+            ),
+            Divider(
+              height: 60.0,
+              color: Colors.blueGrey[300],
+            ),
+            Text(
+              'Name:',
+              style: TextStyle(
+                color: Colors.blueGrey[300],
+                letterSpacing: 2.0,
+                fontSize: 25.0,
+              ),
+            ),
+            SizedBox(height:15.0),
+            Text(
+              'Adekanye Tomi',
+              style: TextStyle(
+                color: Colors.cyanAccent[100],
+                fontSize: 30.0,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0 ),
+            Text(
+              'Marital Status:',
+              style: TextStyle(
+                color: Colors.blueGrey[300],
+                letterSpacing: 2.0,
+                fontSize: 25.0,
+              ),
+            ),
+            SizedBox(height:15.0),
+            Text(
+              'Single',
+              style: TextStyle(
+                color: Colors.cyanAccent[100],
+                fontSize: 30.0,
+                letterSpacing: 2.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 30.0 ),
+            Text(
+              '',
+              style: TextStyle(
+                color: Colors.blueGrey,
+                letterSpacing: 2.0,
+              ),
+            ),
+            SizedBox(height: 10.0 ),
+            Row(
+              children: <Widget>[
+                Icon(
+                  Icons.email,
+                  color: Colors.blueGrey[400],
+                ),
+                SizedBox(width: 10.0),
+                Text(
+                  'adekanyetomy@gmail.com',
+                      style: TextStyle(
+                        color: Colors.blueGrey[400],
+                        fontSize: 18.0,
+                        letterSpacing: 2.0
+
+                      ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
